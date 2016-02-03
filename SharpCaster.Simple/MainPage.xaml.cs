c1e8e81f037f649fb38987c1e3dcf9751ebe8cc5
Teemu Tapanila <Teemu@tapanila.net>
2016-02-03 19:52:46 +0100

    Wrote small sample that shows how to use DeviceLocator

---
+++ SharpCaster.Simple/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using System.Linq;
+
+namespace SharpCaster.Simple
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            var deviceLocator = new DeviceLocator();
+            var devices = await deviceLocator.LocateDevicesAsync(TimeSpan.FromSeconds(2));
+            MessageDialog msg = new MessageDialog($"Found {devices.Count()} chromecasts");
+            await msg.ShowAsync();
+        }
+    }
+}
