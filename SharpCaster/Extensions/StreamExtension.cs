cf51f6c9d8c63872cf8d9955c7b7583fff21cd83
Teemu Tapanila <teemu@tapanila.net>
2016-03-22 10:16:27 +0100

    Updated parsing of messages

--- SharpCaster/Extensions/StreamExtension.cs
+++ SharpCaster/Extensions/StreamExtension.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 
 namespace SharpCaster.Extensions
 {
@@ -8,59 +8,18 @@ namespace SharpCaster.Extensions
     {
         public static IEnumerable<byte> ParseData(this Stream stream)
         {
-            var buffer = new byte[2048];
-
-            var header = new List<byte>();
-            var data = new List<byte>();
-
-
-            var escape = true;
-
-            while (escape)
-            {
-                // tricky byte order for messages
-                var bytesRead = stream.Read(buffer, 0, buffer.Length);
-                if (bytesRead == 1)
-                {
-                    // Incoming series of header /data
-                    header.Add(buffer[0]);
-
-                    bytesRead = stream.Read(buffer, 0, buffer.Length);
-                    if (bytesRead == 3)
-                    {
-                        header.Add(buffer[0]);
-                        header.Add(buffer[1]);
-                        header.Add(buffer[2]);
-
-                        bytesRead = stream.Read(buffer, 0, buffer.Length);
-                        if (bytesRead == 1)
-                        {
-                            header.Add(buffer[0]);
-                            bytesRead = stream.Read(buffer, 0, buffer.Length);
-                            data.AddRange(buffer.Take(bytesRead));
-                            escape = false;
-                        }
-                        else
-                        {
-                            escape = false;
-                        }
-                    }
-                    else
-                    {
-                        escape = false;
-                    }
-                }
-                else
-                {
-                    escape = false;
-                }
-            }
-
-            var entireMessage = new List<byte>();
-            entireMessage.AddRange(header);
-            entireMessage.AddRange(data);
-
-            return entireMessage;
+            var sizeBuffer = new byte[4];
+            byte[] messageBuffer = {};
+            // First message should contain the size of message
+            stream.Read(sizeBuffer, 0, sizeBuffer.Length);
+            // The message is little-endian (that is, little end first),
+            // reverse the byte array.
+            Array.Reverse(sizeBuffer);
+            //Retrieve the size of message
+            var messageSize = BitConverter.ToInt32(sizeBuffer,0);
+            messageBuffer = new byte[messageSize];
+            stream.Read(messageBuffer, 0, messageBuffer.Length);
+            return messageBuffer;
         }
     }
 }
--- SharpCaster/Extensions/StreamSocketExtension.cs
+++ SharpCaster/Extensions/StreamSocketExtension.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Windows.Networking.Sockets;
+﻿using Windows.Networking.Sockets;
 using Windows.Security.Cryptography.Certificates;
 
 namespace SharpCaster.Extensions
