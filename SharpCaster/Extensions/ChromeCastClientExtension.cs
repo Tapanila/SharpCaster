660a4b90b2e20a8da1e4a5bf0d01cf595744ee38
wind-rider <hansmbakker+github@gmail.com>
2016-08-22 00:07:51 +0200

    Plex controller & playback commands for youtube

--- SharpCaster/Extensions/ChromeCastClientExtension.cs
+++ SharpCaster/Extensions/ChromeCastClientExtension.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using SharpCaster.Channels;
 using SharpCaster.Controllers;
+using SharpCaster.Models;
 
 namespace SharpCaster.Extensions
 {
@@ -25,7 +26,20 @@ namespace SharpCaster.Extensions
 
         public static YouTubeChannel GetYouTubeChannel(this IEnumerable<IChromecastChannel> channels)
         {
-            return (YouTubeChannel) channels.First(x => x.Namespace == "urn:x-cast:com.google.youtube.mdx");
+            return (YouTubeChannel)channels.First(x => x.Namespace == MessageFactory.DialConstants.YouTubeUrn);
+        }
+
+        public static async Task<PlexController> LaunchPlex(this ChromeCastClient client)
+        {
+            client.Channels.Add(new PlexChannel(client));
+            var controller = new PlexController(client);
+            await controller.LaunchApplication();
+            return controller;
+        }
+
+        public static PlexChannel GetPlexChannel(this IEnumerable<IChromecastChannel> channels)
+        {
+            return (PlexChannel)channels.First(x => x.Namespace == MessageFactory.DialConstants.PlexUrn);
         }
     }
 }
