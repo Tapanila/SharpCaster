b9921b63caa32987532cf7a6e6893d0806263743
Teemu Tapanila <Teemu@tapanila.net>
2022-02-12 20:51:25 +0200

    fixed capitalization

---
+++ Sharpcaster/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sharpcaster.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static string ToString<T>(this IEnumerable<T> l, string separator)
+        {
+            return "[" + String.Join(separator, l.Select(i => i.ToString()).ToArray()) + "]";
+        }
+    }
+}
