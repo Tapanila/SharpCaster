e7a81177f06ef0c27bce4c7df18c220402369df0
Teemu Tapanila <teemu@tapanila.net>
2024-07-16 22:42:51 +0300

    wrong logger was used for receive payload logging

--- Sharpcaster/ChromeCastClient.cs
+++ Sharpcaster/ChromeCastClient.cs
@@ -152,7 +152,7 @@ namespace Sharpcaster
                             {
                                 GetChannel<IHeartbeatChannel>().StopTimeoutTimer();
                             }
-                            _logger?.LogTrace($"RECEIVED: {payload}");
+                            channel?._logger?.LogTrace($"RECEIVED: {payload}");
                             
                             var message = JsonConvert.DeserializeObject<MessageWithId>(payload);
                             if (MessageTypes.TryGetValue(message.Type, out Type type))
