95d805cdaffa8ca5893386bc869a49e20792b26b
Teemu Tapanila <teemu@tapanila.net>
2024-12-07 15:41:25 +0100

    added the new type to logging tester

--- Sharpcaster.Test/LoggingTester.cs
+++ Sharpcaster.Test/LoggingTester.cs
@@ -20,7 +20,7 @@ namespace Sharpcaster.Test
             List<string> logLines = [];
             _ = TestHelper.GetClientWithTestOutput(output, assertableLog: logLines);
 
-            Assert.Equal("MessageTypes: [addUserResponse,getInfoResponse,LAUNCH_ERROR,RECEIVER_STATUS,QUEUE_CHANGE,QUEUE_ITEM_IDS,QUEUE_ITEMS,DEVICE_UPDATED,MULTIZONE_STATUS,ERROR,INVALID_REQUEST,LOAD_CANCELLED,LOAD_FAILED,MEDIA_STATUS,PING,CLOSE]", logLines[0]);
+            Assert.Equal("MessageTypes: [addUserResponse,getInfoResponse,LAUNCH_ERROR,RECEIVER_STATUS,QUEUE_CHANGE,QUEUE_ITEM_IDS,QUEUE_ITEMS,DEVICE_UPDATED,MULTIZONE_STATUS,ERROR,INVALID_REQUEST,LOAD_CANCELLED,LOAD_FAILED,MEDIA_STATUS,PING,CLOSE,LAUNCH_STATUS]", logLines[0]);
         }
 
         [Theory]
