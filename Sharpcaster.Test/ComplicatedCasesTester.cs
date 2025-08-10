77d307eb90c611ca066019246a639531f1170611
Teemu Tapanila <teemu@tapanila.net>
2025-08-10 22:49:17 +0300

    added two more test cases that are trying to reproduce the issues

--- Sharpcaster.Test/ComplicatedCasesTester.cs
+++ Sharpcaster.Test/ComplicatedCasesTester.cs
@@ -72,5 +72,115 @@ namespace Sharpcaster.Test
 
 
         }
+        [Fact]
+        public async Task TestingVolumeRelatedIssueWithMp3()
+        {
+            var TestHelper = new TestHelper();
+            var client = await TestHelper.CreateConnectAndLoadAppClient(outputHelper, fixture);
+
+            var media = new Media
+            {
+                ContentUrl = "https://incompetech.com/music/royalty-free/mp3-royaltyfree/Arcane.mp3",
+                ContentType = "audio/mpeg",
+                Metadata = new MusicTrackMetadata
+                {
+                    Title = "Arcane",
+                },
+            };
+
+            await client.MediaChannel.LoadAsync(media);
+
+            var status = await client.ReceiverChannel.SetVolume(0.1);
+            Assert.Equal(0.1, status.Volume.Level.Value, precision: 1);
+            var mediaStatus = await client.MediaChannel.SetVolumeAsync(0.8);
+            Assert.Equal(0.8, mediaStatus.Volume.Level.Value, precision: 1);
+            status = await client.ReceiverChannel.SetVolume(0.3);
+            Assert.Equal(0.3, status.Volume.Level.Value, precision: 1);
+            status = await client.ReceiverChannel.SetVolume(0.5);
+            Assert.Equal(0.5, status.Volume.Level.Value, precision: 1);
+            status = await client.ReceiverChannel.SetVolume(0.8);
+            Assert.Equal(0.8, status.Volume.Level.Value, precision: 1);
+
+            await client.MediaChannel.PauseAsync();
+
+            await client.DisconnectAsync();
+
+            await client.ConnectChromecast(fixture.Receivers.FirstOrDefault());
+
+            await client.LaunchApplicationAsync("B3419EF5", true);
+
+            await client.MediaChannel.PlayAsync();
+
+            status = await client.ReceiverChannel.SetVolume(0.1);
+            Assert.Equal(0.1, status.Volume.Level.Value, precision: 1);
+            mediaStatus = await client.MediaChannel.SetVolumeAsync(0.8);
+            Assert.Equal(0.8, mediaStatus.Volume.Level.Value, precision: 1);
+            status = await client.ReceiverChannel.SetVolume(0.3);
+            Assert.Equal(0.3, status.Volume.Level.Value, precision: 1);
+            status = await client.ReceiverChannel.SetVolume(0.5);
+            Assert.Equal(0.5, status.Volume.Level.Value, precision: 1);
+            status = await client.ReceiverChannel.SetVolume(0.8);
+            Assert.Equal(0.8, status.Volume.Level.Value, precision: 1);
+
+
+        }
+
+        [Fact]
+        public async Task TestingWithSwitchingRunningApplication()
+        {
+            var TestHelper = new TestHelper();
+            var client = await TestHelper.CreateConnectAndLoadAppClient(outputHelper, fixture);
+
+            var media = new Media
+            {
+                ContentUrl = "https://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/mp4/DesigningForGoogleCast.mp4",
+                ContentType = "video/mp4",
+                Metadata = new MediaMetadata
+                {
+                    Title = "Designing for Google Cast",
+                    Images = new[]
+                    {
+                        new Image
+                        {
+                            Url = "https://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/poster/DesigningForGoogleCast.jpg"
+                        }
+                    }
+                },
+            };
+
+            await client.MediaChannel.LoadAsync(media);
+
+            var status = await client.ReceiverChannel.SetVolume(0.1);
+            Assert.Equal(0.1, status.Volume.Level.Value, precision: 1);
+            var mediaStatus = await client.MediaChannel.SetVolumeAsync(0.8);
+            Assert.Equal(0.8, mediaStatus.Volume.Level.Value, precision: 1);
+            status = await client.ReceiverChannel.SetVolume(0.3);
+            Assert.Equal(0.3, status.Volume.Level.Value, precision: 1);
+            status = await client.ReceiverChannel.SetVolume(0.5);
+            Assert.Equal(0.5, status.Volume.Level.Value, precision: 1);
+            status = await client.ReceiverChannel.SetVolume(0.8);
+            Assert.Equal(0.8, status.Volume.Level.Value, precision: 1);
+
+            await client.DisconnectAsync();
+
+            await client.ConnectChromecast(fixture.Receivers.FirstOrDefault());
+
+            await client.LaunchApplicationAsync("CC1AD845");
+
+            await client.MediaChannel.LoadAsync(media);
+
+            status = await client.ReceiverChannel.SetVolume(0.1);
+            Assert.Equal(0.1, status.Volume.Level.Value, precision: 1);
+            mediaStatus = await client.MediaChannel.SetVolumeAsync(0.8);
+            Assert.Equal(0.8, mediaStatus.Volume.Level.Value, precision: 1);
+            status = await client.ReceiverChannel.SetVolume(0.3);
+            Assert.Equal(0.3, status.Volume.Level.Value, precision: 1);
+            status = await client.ReceiverChannel.SetVolume(0.5);
+            Assert.Equal(0.5, status.Volume.Level.Value, precision: 1);
+            status = await client.ReceiverChannel.SetVolume(0.8);
+            Assert.Equal(0.8, status.Volume.Level.Value, precision: 1);
+
+
+        }
     }
 }
